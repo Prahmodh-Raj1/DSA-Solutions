4720cf039dbe4fcdedb19d645f4d2c225e0cd053
Prahmodh R <89332177+Prahmodh-R@users.noreply.github.com>
2022-01-26 21:12:40 +0530

    updated alien dictionary.cpp

--- Graphs/Alien dictionary.cpp
+++ Graphs/Alien dictionary.cpp
@@ -1,49 +1,69 @@
 //Problem link: https://www.lintcode.com/problem/892/
 class Solution {
-public:
-  vector<int> toposort(int N, vector<int> adj[]) {
-      queue<char> q; 
-	    vector<int> indegree(N, 0);  //assuming graph is 0-based
-	    for(int i = 0;i<N;i++) {
-	        for(auto it: adj[i]) {  //an adjacent node means there is a node being directed to the current node, so we increment the degree of that node
-	            indegree[it]++;  //calculating how many nodes are incoming to the adjacent ndoe
-	        }
-	    }
-	    
-	    for(int i = 0;i<N;i++) {
-	        if(indegree[i] == 0) {
-	            q.push(i); 
-	        }
-	    }
-	    string topo="";     //storing the topo sort
-	    while(!q.empty()) {
-	        char node = q.front(); 
-	        q.pop(); 
-	        topo+=node
-	        for(auto it : adj[node]) {  //traversing for all it's adjacent nodes
-	            indegree[it]--;
-	            if(indegree[it] == 0) {
-	                q.push(it); 
-	            }
-	        }
-	    }
-	    return topo;
+private:
+	int getInt(char c){
+		return(c-'a');
 	}
+private:
+	bool checkcycle(int node,vector<int> adj[],vector<int> &vis,vector<int> &pathvis,stack<int> &st){
+		vis[node]=1;
+		pathvis[node]=1;
+		for(auto it: adj[node]){
+			if(!vis[it]){
+				if(checkcycle(it,adj,vis,it)==true) return true;
+			}
+			else if(!pathvis[it]){
+				return true;
+			}
+		}
+		pathvis[node]=0;
+		st.push(node);
+		return false;
+	}
+public:
+ 	//Edge cases:
+	//if no mismatch is found, then longer word comes after shorter word
+	//if there is a cycle in the Directed graph
   string alienOrder(vector<string> &words) {
-        vector<vector<int>> adj(k);
-        for(int i=0;i<n-1;i++){
-          string word1=words[i];
-          string word2=words[i+1];
-          int minl=min(word1.size(),word2.size());
-          int cnt=0;
-          for(int j=0;j<minl;j++){
-            if(word1[j]!=word2[j]){
-              cnt++;
-              adj[word1[j]-'a'].push_back(word2[j]-'a');
-              break;
-            }
-          }
-        }
-        cout<<toposort(cnt,g);
+ 	int n=words.size();
+	  //a:0, b:1, ...z:25
+	vector<int> adj[26];
+	vector<int> store(26,0),vis(26,0),pathvis(26,0);
+	for(int i=0;i<n;i++){
+		  for(auto it: words[i]){
+			  store[getint(it)]=1;
+		  }
+	}
+	for(int i=0;i<n;i++){
+		string s1=words[i];
+		string s2=words[i+1];
+		for(int j=0;j<n;j++){
+			if(j>=s2.size()){
+				return "";
+			}
+			if(s1[j]!=s2[j]){
+				store[getint(s1[j])]=1;
+				store[getint(s2[j])]=1;
+				adj[getint(s1[j])].push_back(getint(s2[j]));
+				
+				break;
+			}
+		}
+		
+	}
+	stack<int> st;
+	for(int i=0;i<26;i++){
+		if(store[i] && !vis[i]){
+			if(checkcycle(i,adj,vis,pathvis,st)==true){
+				return "";
+			}
+		}
+	}
+	string ans="";
+	while(!st.empty()){
+		ans+=char(st.top() + 'a');
+		st.pop();
+	}
+	return ans;
    }
 };
