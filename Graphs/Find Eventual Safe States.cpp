8235e94326ff41ba2e893bf9a7b34ec12ae62f6f
Prahmodh R <89332177+Prahmodh-R@users.noreply.github.com>
2022-10-25 19:20:55 +0530

    Create Find Eventual Safe States.cpp

---
+++ Graphs/Find Eventual Safe States.cpp
@@ -0,0 +1,39 @@
+//Problem link: https://leetcode.com/problems/find-eventual-safe-states/
+//TC : O(V+E) SC:O(V+E)
+
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //Reverse all the edges
+        int n=graph.size();
+        vector<int> indegree(n,0);
+        vector<int> adj[n];
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                indegree[i]++;
+                adj[it].push_back(i);
+            }
+        }
+        vector<int> ans;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+              
+              q.push(i);
+            } 
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
