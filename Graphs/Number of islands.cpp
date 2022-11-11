d2b5638e8ae0d90cf7bf822ca0fb303e6aa9c5e6
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2022-11-11 12:14:27 +0530

    Update Number of islands.cpp

--- Graphs/Number of islands.cpp
+++ Graphs/Number of islands.cpp
@@ -1,6 +1,8 @@
 //Problem link: 
 //TC :O(M*N) SC:O(M*N)
 
+
+//Using DFS
 class Solution {
 public:
     bool vis[300][300];
@@ -35,6 +37,56 @@ public:
                     vis[i][j]=true;
                     
                     dfs(i,j,grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+//Using BFS:
+class Solution {
+public:
+    bool vis[300][300];
+    int di[4]={-1,0,1,0};
+    int dj[4]={0,1,0,-1};
+    bool isvalid(int i,int j,int m,int n){
+        return i>=0 and i<m and j>=0 and j<n;
+    }
+    void bfs(int i,int j,vector<vector<char>>& grid){
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        vis[i][j]=true;
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            
+            q.pop();
+        
+            for(int k=0;k<4;k++){
+                int ni=x+di[k];
+                int nj=y+dj[k];
+                if(isvalid(ni,nj,grid.size(),grid[0].size()) and !vis[ni][nj]){
+                    if(grid[ni][nj]=='1'){
+                        vis[ni][nj]=true;
+                        q.push({ni,nj});
+                    }
+                
+                }
+             }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        memset(vis,false,sizeof(vis));
+        int m=grid.size();
+        int n=grid[0].size();
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' and !vis[i][j]){
+                    cnt++;
+                    
+                    bfs(i,j,grid);
                 }
             }
         }
