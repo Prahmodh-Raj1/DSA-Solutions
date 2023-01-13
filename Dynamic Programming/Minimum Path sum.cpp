6b01b88f34d9926b5de21c2f740b1af9672c6b26
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-13 15:01:50 +0530

    Updated minimum path sum-leetcode

--- Dynamic Programming/Minimum Path sum.cpp
+++ Dynamic Programming/Minimum Path sum.cpp
@@ -16,3 +16,35 @@ public:
         return summ(grid,grid.size()-1,grid[0].size()-1);
     }
 };
+
+//Space optimised approach
+//TC:O(N^2) SC:O(N)
+
+int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+       
+        vector<int> prev(n,0);
+
+        for(int i=0;i<m;i++){
+            vector<int> cur(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 and j==0){
+                    cur[j]=grid[i][j];
+                }
+                else{
+                    int up=grid[i][j],left=grid[i][j];
+                    if(i>0){  
+                        up+=prev[j];
+                    }else{
+                        up+=1e9;
+                    }
+                    if(j>0) left+=cur[j-1];  
+                    else left+=1e9;
+                    cur[j]=min(up,left);
+                }
+            }
+            prev=cur;
+        }
+        return prev[n-1];
+    }
