e3f2a776b8b5c6c55c7c693b87c4782f39207662
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-16 11:49:38 +0530

    Create Partition with given difference.cpp

---
+++ Dynamic Programming/Partition with given difference.cpp
@@ -0,0 +1,32 @@
+//TC :O(N^2) SC:O(N)
+int findWays(vector<int> &num, int k){
+    int n = num.size();
+
+    vector<int> prev(k+1,0);
+    
+    prev[0] =1;
+    
+    if(num[0]<=k)
+        prev[num[0]] = 1;
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<int> cur(k+1,0);
+        cur[0]=1;
+        for(int target= 1; target<=k; target++){
+            
+            int notTaken = prev[target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = prev[target-num[ind]];
+        
+            cur[target]= notTaken + taken;
+        }
+        
+        prev = cur;
+    }
+    
+    return prev[k];
+
+    
+}
