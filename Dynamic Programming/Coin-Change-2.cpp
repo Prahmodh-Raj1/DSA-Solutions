48240c113c0183b6d4061bdc8abdc133fbffd344
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-18 17:18:36 +0530

    Update Coin-Change-2.cpp

--- Dynamic Programming/Coin-Change-2.cpp
+++ Dynamic Programming/Coin-Change-2.cpp
@@ -16,3 +16,30 @@ public:
         return dp[amount];
     }
 };
+
+
+//TC:O(N*Amount) SC:O(Amount)
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+       // vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        
+        vector<int> prev(amount+1,0),cur(amount+1);
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0) prev[t]=1;
+            else prev[t]=0;
+            
+        }
+        for(int i=1;i<n;i++){
+            for(int a=0;a<=amount;a++){
+                int nottake=prev[a];
+                int take=0;
+                if(coins[i]<=a) take=cur[a-coins[i]];
+                cur[a]=(take+nottake);
+            }
+            prev=cur;
+        }
+        return prev[amount];
+    }
+};
