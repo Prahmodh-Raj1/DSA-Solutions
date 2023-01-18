9edeccbb126cd8a45a6fd99bd7a0e49305195e53
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-18 21:48:32 +0530

    Update LCS.cpp

--- Dynamic Programming/LCS.cpp
+++ Dynamic Programming/LCS.cpp
@@ -47,3 +47,29 @@ public:
         return dp[n][m];
     }
 };
+
+//Space optimised soln SC:O(2*N)
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<int> prev(m+1,0),cur(m+1,0);
+        
+        for(int i=0;i<=m;i++) prev[i]=0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    cur[j]=1+prev[j-1];
+                }else{
+                    cur[j]=max(prev[j],cur[j-1]);
+                }
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+};
+
