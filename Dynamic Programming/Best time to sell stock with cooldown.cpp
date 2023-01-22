57348f40ecbd645e6a03275e1d98749ef6577d21
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-22 13:31:55 +0530

    Create Best time to sell stock with cooldown.cpp

---
+++ Dynamic Programming/Best time to sell stock with cooldown.cpp
@@ -0,0 +1,39 @@
+//Memoization: TC:O(N*2) SC:O(N*2) + Auxilliary stack space
+//Tabulation: TC:O(N*2) SC:O(N*2)
+class Solution {
+public:
+    int func(int i,int canbuy,int n,vector<int>& prices,vector<vector<int>>& dp){
+        if(i>=n) return 0;
+        if(dp[i][canbuy]!=-1) return dp[i][canbuy];
+        int profit=0;
+        if(canbuy){
+            int take=-1*prices[i] + func(i+1,0,n,prices,dp);
+            int nottake=func(i+1,1,n,prices,dp);  //not selling, so we can buy
+            profit=max(take,nottake);
+        }
+        else{
+            profit=max(prices[i]+func(i+2,1,n,prices,dp),func(i+1,0,n,prices,dp));
+        }
+        return dp[i][canbuy]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        //vector<int> prev(3,0),cur(3,0);
+
+        int profit;
+        for(int i=n-1;i>=0;i--){ //opp to the memoization flow
+            for(int b=0;b<2;b++){
+                profit=0;
+                if(b){
+                  profit=max(dp[i+1][0]-prices[i],dp[i+1][1]);  
+                }else{
+                    profit=max(dp[i+2][1]+prices[i],dp[i+1][0]);
+                }
+                dp[i][b]=profit;
+            }
+            
+        }
+        return dp[0][1]; 
+    }
+};
