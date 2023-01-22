eda2e8204b55eae2ae5fb62980553af172642cb7
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-22 13:36:28 +0530

    Update Best time to sell stock with cooldown.cpp

--- Dynamic Programming/Best time to sell stock with cooldown.cpp
+++ Dynamic Programming/Best time to sell stock with cooldown.cpp
@@ -37,3 +37,34 @@ public:
         return dp[0][1]; 
     }
 };
+
+//Space optimization
+    int maxProfit(vector<int>& Arr) {
+        int n=Arr.size();
+    vector<int> cur(2,0);
+    vector<int> front1(2,0);
+    vector<int> front2(2,0);
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit;
+            
+            if(buy==0){// We can buy the stock
+                profit = max(0+front1[0], -Arr[ind] + front1[1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+front1[1], Arr[ind] + front2[0]);
+            }
+            
+            cur[buy] = profit;
+        }
+        
+        front2 = front1;
+        front1 = cur;
+        
+    }
+    
+    return cur[0];
+ 
+    }
