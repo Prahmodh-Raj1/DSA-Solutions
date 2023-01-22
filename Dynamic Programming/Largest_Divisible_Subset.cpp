a9816d26bbb741696fe7d525c59198f93fcfbfc1
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-22 21:58:39 +0530

    Update Largest_Divisible_Subset.cpp

--- Dynamic Programming/Largest_Divisible_Subset.cpp
+++ Dynamic Programming/Largest_Divisible_Subset.cpp
@@ -35,3 +35,33 @@ public:
         return ans;
     }
 };
+
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        //find the longest divisibe subsequence
+        vector<int> dp(n,1),hash(n);
+        int last=0;
+        int maxi=-1;
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]%nums[prev]==0 and 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev;
+                }
+            
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                last=i;
+            }
+        }
+        vector<int> temp;
+        temp.push_back(nums[last]);
+        while(hash[last]!=last){
+            last=hash[last];
+            temp.push_back(nums[last]);
+        }
+        return temp;
+    }
