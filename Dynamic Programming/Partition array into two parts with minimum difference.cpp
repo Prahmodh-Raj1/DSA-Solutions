9a774a4408ebd1a422fd7b843922edd4464eb04d
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-01-23 20:37:58 +0530

    Create Partition array into two parts with minimum difference.cpp

---
+++ Dynamic Programming/Partition array into two parts with minimum difference.cpp
@@ -0,0 +1,47 @@
+
+//Problem link: https://leetcode.com/problems/last-stone-weight-ii/description/
+
+bool subsetSumUtil(int ind, int target, vector < int > & arr, vector < vector 
+< int >> & dp) {
+  if (target == 0)
+    return dp[ind][target]=true;
+
+  if (ind == 0)
+    return dp[ind][target] = arr[0] == target;
+
+  if (dp[ind][target] != -1)
+    return dp[ind][target];
+
+  bool notTaken = subsetSumUtil(ind - 1, target, arr, dp);
+
+  bool taken = false;
+  if (arr[ind] <= target)
+    taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
+
+  return dp[ind][target] = notTaken || taken;
+}
+
+int minSubsetSumDifference(vector < int > & arr, int n) {
+
+  int totSum = 0;
+
+  for (int i = 0; i < n; i++) {
+    totSum += arr[i];
+  }
+
+  vector < vector < int >> dp(n, vector < int > (totSum + 1, -1));
+
+  for (int i = 0; i <= totSum; i++) {
+    bool dummy = subsetSumUtil(n - 1, i, arr, dp);
+  }
+
+  int mini = 1e9;
+  for (int i = 0; i <= totSum; i++) {
+    if (dp[n - 1][i] == true) {
+      int diff = abs(i - (totSum - i));
+      mini = min(mini, diff);
+    }
+  }
+  return mini;
+
+}
