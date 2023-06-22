8c30aa44735c2637eceeb73623f0bc5a16273041
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-06-22 18:20:11 +0530

    Create Find First and Last position.cpp

---
+++ Binary Search/Find First and Last position.cpp
@@ -0,0 +1,25 @@
+//Problem link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+//TC:O(2*LOGN) SC:O(1)
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+    int idx1 = lower_bound(nums, target);
+    int idx2 = lower_bound(nums, target+1)-1;
+    if (idx1 < nums.size() && nums[idx1] == target)
+        return {idx1, idx2};
+    else
+        return {-1, -1};
+}
+
+int lower_bound(vector<int>& nums, int target) {
+    int l = 0, r = nums.size()-1;
+    while (l <= r) {
+        int mid = (r-l)/2+l;
+        if (nums[mid] < target)
+            l = mid+1;
+        else
+            r = mid-1;
+    }
+    return l;
+}
+};
