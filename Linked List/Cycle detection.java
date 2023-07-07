98982aaada65f77d8ecde547dba3d0959a0e7c8b
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-07-07 10:54:53 +0530

    Create Cycle detection.java

---
+++ Linked List/Cycle detection.java
@@ -0,0 +1,17 @@
+//Problem link: https://leetcode.com/problems/linked-list-cycle
+//TC: O(N) SC: O(1)
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
