73dc6f5ecc328a275c674830822a661d17906c20
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-07-08 17:25:52 +0530

    Update next greater element.cpp

--- Stacks & queues/next greater element.cpp
+++ Stacks & queues/next greater element.cpp
@@ -1,3 +1,4 @@
+//Circular array:
 #include<bits/stdc++.h>
 using namespace std;
 //brute force
@@ -55,3 +56,29 @@ int main(){
 	return 0;
 
 }
+
+//Non-circular array:
+vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long> ans(n,-1);
+        stack<long long> st;
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() and st.top()<=arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i]=st.top();
+            }else{
+                ans[i]=-1;
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+
+/*intuition:
+ Let’s say we are checking for an element at index i. We keep popping from the stack until the element at the top of the stack is smaller than A[i]. The main intuition behind popping them is that these elements can never
+be the NGE for any element present at the left of A[i] because A[i] is greater than these elements. Now, if the top element of S is greater than A[i] then this
+is NGE of A[i] and we will assign it to res[i], where res is the resultant array. If the stack becomes empty then it implies that no element at the right of A[i] 
+is greater than it and we assign -1. At last, we push A[i] in S.*/
