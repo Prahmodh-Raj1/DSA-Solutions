55c3616468adf4b3a52690392858a41d749aec31
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-07-13 22:01:32 +0530

    Create Valid Palindrome 2.cpp

---
+++ Two pointers/Valid Palindrome 2.cpp
@@ -0,0 +1,28 @@
+//problem link: https://leetcode.com/problems/valid-palindrome-ii
+//TC: O(N) SC:O(1)
+class Solution {
+bool ispalindrome(string s,int l,int r){
+    while(l<r){
+        if(s[l]!=s[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+public:
+    bool validPalindrome(string s) {
+        int i=0, j=s.size()-1;
+        //atmost one character mismatch can happen
+        bool fl=false;
+        while(i<j){
+            if(s[i]!=s[j]){
+                //REMAINING LENGTH SHOULD BE A PALINDROME
+                return ispalindrome(s,i+1,j) || ispalindrome(s,i,j-1);
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
