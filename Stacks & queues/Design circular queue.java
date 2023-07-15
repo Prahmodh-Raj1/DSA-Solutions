2af442054f389a5b9510e76bac2212f471178f03
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-07-15 18:36:16 +0530

    Create Design circular queue.java

---
+++ Stacks & queues/Design circular queue.java
@@ -0,0 +1,33 @@
+//Problem link: https://leetcode.com/problems/design-circular-queue/
+//TC: O(N) SC: O(1)
+class MyCircularQueue {
+        int[] a;
+        int front = 0, rear = -1, len = 0;
+
+        public MyCircularQueue(int k) { a = new int[k];}
+
+        public boolean enQueue(int val) {
+            if (!isFull()) {
+                rear = (rear + 1) % a.length;
+                a[rear] = val;
+                len++;
+                return true;
+            } else return false;
+        }
+
+        public boolean deQueue() {
+            if (!isEmpty()) {
+                front = (front + 1) % a.length;
+                len--;
+                return true;
+            } else return false;
+        }
+
+        public int Front() { return isEmpty() ? -1 : a[front];}
+
+        public int Rear() {return isEmpty() ? -1 : a[rear];}
+
+        public boolean isEmpty() { return len == 0;}
+
+        public boolean isFull() { return len == a.length;}
+    }
