a450de5fcd6d9dec2e612f196ec44dc50854e5d0
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-08-05 12:41:38 +0530

    Create Meeting rooms.cpp

---
+++ Intervals Pattern/Meeting rooms.cpp
@@ -0,0 +1,23 @@
+//Problem link: https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1?
+static bool comp(pair<int,int>& p1,pair<int,int>& p2){
+        return p1.second<p2.second;
+    }
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        vector<pair<int,int>> times;
+        for(int i=0;i<n;i++){
+            times.push_back({start[i],end[i]});
+        }
+        sort(times.begin(),times.end(),comp);
+        int cnt=1;
+        int prevend = times[0].second;
+        for(int i=1;i<n;i++){
+            if(times[i].first>prevend){
+                prevend = times[i].second;
+                cnt++;
+            }
+        }
+        return cnt;
+        
+    }
