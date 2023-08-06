c5b7e758c4a6a0a809ecef332fa6d402352b3820
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-08-06 18:45:26 +0530

    Create Largest rectangle in histogram.cpp

---
+++ Stacks & queues/Monotonic Stack Pattern/Largest rectangle in histogram.cpp
@@ -0,0 +1,24 @@
+//problem link: https://leetcode.com/problems/largest-rectangle-in-histogram
+//TC: O(N) SC: O(N)
+Area: arr[i] * (right smaller – left smaller -1 ).
+  class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxarea=0;
+        int n=heights.size();
+        //we use the idea of a previous smaller element 
+        for(int i=0;i<=n;i++){ //we do i<=n to ensure the case of continuously increasing array
+            while(!st.empty() and (i==n || heights[st.top()]>=heights[i])){
+                int length=heights[st.top()];
+                st.pop();
+                int breadth;
+                if(st.empty()) breadth=i;
+                else breadth=i-st.top()-1;
+                maxarea=max(length*breadth,maxarea);
+            }
+            st.push(i);
+        }
+        return  maxarea;
+    }
+};
