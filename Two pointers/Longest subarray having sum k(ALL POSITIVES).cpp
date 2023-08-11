f8808c03a43524d6b3e1d796b4e7296b15a5166b
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-08-11 08:37:54 +0530

    Create Longest subarray having sum k(ALL POSITIVES).cpp

---
+++ Two pointers/Longest subarray having sum k(ALL POSITIVES).cpp
@@ -0,0 +1,25 @@
+https://takeuforward.org/data-structure/longest-subarray-with-given-sum-k/
+//TC: O(N) SC: O(1)
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
+    }
