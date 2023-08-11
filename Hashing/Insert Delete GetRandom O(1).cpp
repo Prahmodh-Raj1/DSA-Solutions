ecedc1628a025f62a7ba92798e9f1925aa26222e
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-08-11 10:40:25 +0530

    Update Insert Delete GetRandom O(1).cpp

--- Hashing/Insert Delete GetRandom O(1).cpp
+++ Hashing/Insert Delete GetRandom O(1).cpp
@@ -57,3 +57,55 @@ Refer problem : https://leetcode.com/problems/insert-delete-getrandom-o1/
  * bool param_2 = obj->remove(val);
  * int param_3 = obj->getRandom();
  */
+
+//part 2: https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed
+/*
+There are two data member in the solution
+
+a vector nums
+an unordered_map m
+The key of m is the val, the value of m is a vector contains indices where the val appears in nums.
+Each element of nums is a pair, the first element of the pair is val itself, the second element of the pair is an index into m[val].
+There is a relationship between nums and m:
+
+m[nums[i].first][nums[i].second] == i;*/
+class RandomizedCollection {
+public:
+    /** Initialize your data structure here. */
+    RandomizedCollection() {
+        
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    bool insert(int val) {
+        auto result = m.find(val) == m.end();
+        
+        m[val].push_back(nums.size());
+        nums.push_back(pair<int, int>(val, m[val].size() - 1));
+        
+        return result;
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    bool remove(int val) {
+        auto result = m.find(val) != m.end();
+        if(result)
+        {
+            auto last = nums.back();
+            m[last.first][last.second] = m[val].back();
+            nums[m[val].back()] = last;
+            m[val].pop_back();
+            if(m[val].empty()) m.erase(val);
+            nums.pop_back();
+        }
+        return result;
+    }
+    
+    /** Get a random element from the collection. */
+    int getRandom() {
+        return nums[rand() % nums.size()].first;
+    }
+private:
+    vector<pair<int, int>> nums;
+    unordered_map<int, vector<int>> m;
+};
