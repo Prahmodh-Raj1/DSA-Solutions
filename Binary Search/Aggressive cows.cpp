c6ed9ccd338af5cadca7ca6dc6f2a762ad5f6d36
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-08-14 20:49:16 +0530

    Create Aggressive cows.cpp

---
+++ Binary Search/Aggressive cows.cpp
@@ -0,0 +1,39 @@
+//Prob link: https://www.codingninjas.com/studio/problems/aggressive-cows_1082559?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse&leftPanelTab=0
+//TC: O(NlogN)
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1, high = stalls[n - 1] - stalls[0];
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (canWePlace(stalls, mid, k) == true) {
+            low = mid + 1;
+        }
+        else high = mid - 1;
+    }
+    return high;
+}
+
+
