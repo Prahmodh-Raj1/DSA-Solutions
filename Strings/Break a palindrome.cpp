b9654ffa1871140eb4b5df7939cdc627e1627702
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-08-16 21:25:43 +0530

    Create Break a palindrome.cpp

---
+++ Strings/Break a palindrome.cpp
@@ -0,0 +1,16 @@
+//Prob link: https://leetcode.com/problems/break-a-palindrome
+//TC: O(N) SC: o(1)
+class Solution {
+public:
+        string breakPalindrome(string S) {
+        int n = S.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (S[i] != 'a') {
+                S[i] = 'a';
+                return S;
+            }
+        }
+        S[n - 1] = 'b';
+        return n < 2 ? "" : S;
+    }
+};
